2968094bc15466575aa65f16f1e11d3fb772e582
AndyMasc <andy.mascarenhas12@gmail.com>
2025-06-06 17:13:55 +1200

    Added more relevant file extensions per folder

--- main.py
+++ main.py
@@ -4,14 +4,9 @@ downloadFolder = "/Users/andymascarenhas/Downloads/"
 
 folderTypes = {"Downloaded Images": [".png", ".jpg", ".jpeg", ".gif", ".webp", ".bmp", ".tiff", ".svg", ".heic", ".ico"],
                "Downloaded Videos": [".mp4", ".mov", ".avi", ".mkv", ".webm", ".flv", ".wmv", ".mpeg", ".3gp"],
-               "Donwload Audio Files": [".mp3", ".wav", ".aac", ".flac", ".ogg", ".m4a", ".wma"],
-               "Downloaded Documents": [ ".pdf", ".doc", ".docx", ".txt", ".rtf", ".odt", ".tex", ".md", ".epub"],
-               "Downloaded Spreadsheets": [".xls", ".xlsx", ".ods", ".csv"],
-               "Downloaded Presentations": [".ppt", ".pptx", ".odp"],
-               "Downloaded Code Files": [".py", ".js", ".java", ".cpp", ".c", ".cs", ".html", ".css", ".json", ".xml", ".php", ".sh", ".ts", ".rb", ".go",".ipynb"],
-               "Downloaded Print Files": [".stl", ".gcode", ".3mf", ".obj"],
-               "Downloaded Archives": [".zip", ".rar", ".7z", ".tar", ".gz", ".bz2", ".xz", ".iso"],
-               "Downloaded Installers": [".exe", ".msi", ".dmg", ".pkg", ".deb", ".rpm", ".appimage"]}
+               "Donwload Audio Files": [".mp3", ".wav", ".aac", ".flac", ".wma"],
+               "Downloaded Documents": [ ".pdf", ".doc", ".docx", ".txt", ".rtf"],
+               "Downloaded Print Files": [".stl", ".gcode", ".3mf", ".obj"]}
 
 os.chdir(downloadFolder)
 
