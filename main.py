0a4b822b75dbca64844a56b17fd16307b2432251
AndyMasc <andy.mascarenhas12@gmail.com>
2026-05-10 10:27:03 +1200

    Fix typo in folder creation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ downloadFolder = "/Users/andymascarenhas/Downloads/"
 
 folderTypes = {"Downloaded Images": [".png", ".jpg", ".jpeg", ".gif", ".webp", ".bmp", ".tiff", ".svg", ".heic", ".ico"],
                "Downloaded Videos": [".mp4", ".mov", ".avi", ".mkv", ".webm", ".flv", ".wmv", ".mpeg", ".3gp"],
-               "Donwload Audio Files": [".mp3", ".wav", ".aac", ".flac", ".wma"],
+               "Downloaded Audio Files": [".mp3", ".wav", ".aac", ".flac", ".wma"],
                "Downloaded Documents": [ ".pdf", ".doc", ".docx", ".txt", ".rtf"],
                "Downloaded Print Files": [".stl", ".gcode", ".3mf", ".obj"]}
 
